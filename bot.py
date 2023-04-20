1f05433e3f9f8d6e309c3e94e63a2f0777cbf697
judog24 <judog24@gmail.com>
2023-04-20 16:25:12 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,3 +4,4 @@ from datetime import datetime
 mastodon = Mastodon(access_token="festival_usercred.secret")
 
 #mastodon.status_post(status='Test', scheduled_at=datetime.fromtimestamp(1681509013.0))
+mastodon.status_post(status='Test', scheduled_at=datetime.fromtimestamp(1682088621.0))
