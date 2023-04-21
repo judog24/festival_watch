de10a7e49e3301916505656345eea132f839d5b1
judog24 <judog24@gmail.com>
2023-04-20 17:06:36 -0700

    Update action_bot.py

--- action_bot.py
+++ action_bot.py
@@ -3,6 +3,15 @@ from mastodon import Mastodon
 from datetime import datetime
 
 token = os.environ['FESTIVAL_WATCH_BOT']
-mastodon = Mastodon(access_token=token)
+secret_api_base_url = os.environ['API_BASE_URL']
+secret_client_id = os.environ['CLIENT_ID']
+secret_client_secret = os.environ['CLIENT_SECRET']
+secret_access_token = os.environ['ACCESS_TOKEN']
+
+mastodon = Mastodon(
+    api_base_url = secret_api_base_url,
+    client_id = secret_client_id,
+    client_secret = secret_client_secret,
+    access_token = secret_access_token)
 
 mastodon.status_post(status='Test', scheduled_at=datetime.fromtimestamp(1682088621.0))
